c555e98abff5f5be09b71ffd4ae312c23056bc2d
EzzPrograms <ezzprograms+dev@gmail.com>
2019-02-19 07:03:56 +0000

    Added selinux related edits

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 3 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#3 SMP PREEMPT Tue Feb 12 13:52:20 UTC 2019"
-#define LINUX_COMPILE_BY "EzzDev"
-#define LINUX_COMPILE_HOST "android-build"
-#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
