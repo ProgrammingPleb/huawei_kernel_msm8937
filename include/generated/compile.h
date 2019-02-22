e55d8c99a375ff2e3d0af41a537a416420438ece
EzzPrograms <ezzprograms+dev@gmail.com>
2019-02-22 08:24:32 +0000

    Latest commit before purging build system

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#2 SMP PREEMPT Tue Feb 19 08:48:34 UTC 2019"
+#define LINUX_COMPILE_BY "EzzDev"
+#define LINUX_COMPILE_HOST "android-build"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
